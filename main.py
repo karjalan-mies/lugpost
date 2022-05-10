8d397dd52e27dfb605b401c5a4aa29c32a0348e6
Denis Trifonov <trifonov.d.a@yandex.ru>
2022-05-10 13:17:37 +0300

    Refactoring

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from wsgiref import headers
 import requests
 from bs4 import BeautifulSoup
 
+from test import get_people_data
+
 
 def get_html(url):
     try:
@@ -13,23 +15,24 @@ def get_html(url):
         return False
 
 
-def get_full_name(people):
+def get_people_link(people) -> str:
     link = people.find('a', class_='news-item-title2')['href']
     return link
 
 
-def get_data(html):
-    with open(html, 'r') as f:
-        data = f.read()
+def get_data(data):
+    all_data = []
+    with open(data, 'r') as f:
+        html = f.read()
 
-    soup = BeautifulSoup(data, 'html.parser')
-    peoples = soup.find('div',
-                        id='dle-content').findAll('div',
-                                                  class_='news-item2 clearfix')
-    for people in peoples:
-        link = get_full_name(people)
-        print(link)
-    # pprint(peoples)
+    soup = BeautifulSoup(html, 'html.parser')
+    people = soup.find('div',
+                       id='dle-content').findAll('div',
+                                                 class_='news-item2 clearfix')
+    for person in people:
+        link = get_people_link(person)
+        all_data.append(get_people_data(link))
+    print(all_data)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+from transliterate import translit
 
 headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
       }
+
+
+def translit_it(text: str)-> str:
+    return translit(text, language_code="ru", reversed=True)
+
+
+def process_text(text: str) -> str:
+    text = translit_it(text)
+    return text.lower().replace(':', '').strip().replace(' ', '_')
 
 
 def get_html(url):
@@ -16,28 +26,41 @@ def get_html(url):
         return False
 
 
-def get_people_data():
-    with open('people_data.html', 'r') as f:
-        people_data = f.read()
-
-    soup = BeautifulSoup(people_data, 'html.parser')
-    
+def get_and_save_image(soup):
     img_url = soup.find('img', class_='xfieldimage foto', src=True)['src']
     img_file = requests.get(f'http://lugpost.ru{img_url}')
     file_name = img_url.split('/')[-1]
     with open(file_name, 'wb') as f:
         f.write(img_file.content)
-    l_name, f_name, patronymic = soup.find('h1').text.split()
-    
-    people_info = soup.find('div',
-                            class_='full-text video-box clearfix')
-    people_profile = people_info.findAll('b')
+    return file_name
+
+
+def get_profile_data(soup):
+    people_profile = soup.find('div', class_='full-text video-box clearfix')
+    people_profile = people_profile.findAll('b')
+    profile = {}
     for element in people_profile:
         if element.text and element.find_next('font').text:
-            element_value = element.find_next('font').text
-            print(f'{element.text} {element_value}')
-    print(f'Имя файла: {file_name}')
-    print(f'Фамилия: {l_name}\nИмя: {f_name}\nОтчество: {patronymic}')
+            profile[process_text(element.text)] = element.find_next('font').text
+    return profile
+
+
+def get_people_data(link):
+    # with open('people_data.html', 'r') as f:
+    #     people_data = f.read()
+    people_data = requests.get(link, headers=headers)
+    print(people_data)
+    soup = BeautifulSoup(people_data.text, 'html.parser')
+    # file_name = get_and_save_image(soup)
+    l_name, f_name, patronymic = soup.find('h1').text.split()
+    people_info = {}
+    people_info['last_name'] = l_name
+    people_info['first_name'] = f_name
+    people_info['patronymic'] = patronymic    
+    profile = get_profile_data(soup)
+    people_info = {**people_info, **profile}
+    # people_info['file_name'] = file_name
+    return people_info
 
 
 if __name__ == '__main__':
