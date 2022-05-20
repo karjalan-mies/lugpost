9f7b5673ce9e6b8e4e0c398b7ea983f45a7189f0
Denis Trifonov <trifonov.d.a@yandex.ru>
2022-05-20 10:28:03 +0300

    Modified config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0)' +
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0)'+
                       'Gecko/20100101 Firefox/45.0'
           }
+
 FIELDS = ['data_sozdaniya', 'istochnik_informacii', 'familiya', 'imya',
           'otchestvo', 'pol', 'prezhnyaya_familiya', 'data_rozhdenija',
           'mesto_rozhdenija', 'grazhdanstvo', 'vod_ud', 'zagr_passport',
           'passp_rf_seriya_nomer', 'passp_rf_data_vidachi',
           'passp_rf_kem_vydan', 'adres_strana_prozh', 'adres_reg_prozh',
           'adres_np', 'adres_ulica', 'adres_dom', 'adres_stroen_korp',
-          'adres_kvart', 'telefon', 'imya_faila', 'papka']
+          'adres_kvart', 'telefon', 'imya_faila', 'papka',]
 
 URL = 'https://lugpost.ru/vprestupleniya/'
